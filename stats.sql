dac046e556fa1268c4cb8b33959e9cee661381ed
peterHeuz <jan-peter.heuzeroth@student.hpi.uni-potsdam.de>
2015-06-14 18:24:45 +0200

    statistics about joined table added

--- stats.sql
+++ stats.sql
@@ -1,5 +1,7 @@
 DELIMITER //
 
+# queried tables should be cs_join_md5 in the end
+
 DROP PROCEDURE IF EXISTS CalculateStatistics;
 
 # creates rudimentary stats (#categories, #subjects, avg #subjects in categories)
@@ -9,14 +11,22 @@ CREATE PROCEDURE CalculateStatistics()
 	BEGIN
 		DECLARE amount_categories INT DEFAULT 0;
 		DECLARE amount_subjects INT DEFAULT 0;
-		DECLARE average_subjectsInCategories FLOAT 0.0;
+		DECLARE amount_predicates INT DEFAULT 0;
+		DECLARE amount_objects INT DEFAULT 0;
+		DECLARE average_subjectsInCategories FLOAT DEFAULT 0.0;
+		DECLARE average_categoriesPerSubject FLOAT DEFAULT 0.0;
+		DECLARE average_statementsPerSubject FLOAT DEFAULT 0.0;
 		
 		DROP TABLE IF EXISTS stats;
 		CREATE TABLE stats (
 			dataset_table VARCHAR(60),
 			a_categories int,
 			a_subjects int,
-			avg_subjectsInCategories int);
+			a_predicates int,
+			a_objects int,
+			avg_subjectsInCategories float,
+			avg_categoriesPerSubject float,
+			avg_statementsPerSubject float);
 
 		SELECT count(*) INTO amount_categories
 		FROM (
@@ -28,10 +38,30 @@ CREATE PROCEDURE CalculateStatistics()
 			SELECT DISTINCT subject
 			FROM cs_join_original) dist_subs;
 
+		SELECT count(*) INTO amount_predicates
+		FROM (
+			SELECT DISTINCT predicate
+			FROM cs_join_original) dist_preds;
+
+		SELECT count(*) INTO amount_objects
+		FROM (
+			SELECT DISTINCT object
+			FROM cs_join_original) dist_objs;
+
 		SET average_subjectsInCategories = amount_subjects / amount_categories;
+		SET average_categoriesPerSubject = amount_categories / amount_subjects;
+			
+		SELECT AVG(subcounts) INTO average_statementsPerSubject
+		FROM (
+			SELECT count(subject) AS subcounts
+			FROM cs_join_original
+			GROUP BY subject) counts;
+
 
 		INSERT INTO stats
-		VALUES ("cs_join", amount_categories, amount_subjects, average_subjectsInCategories);
-	END
+		VALUES ("cs_join", amount_categories, amount_subjects, 
+			amount_predicates, amount_objects, average_subjectsIncategories,
+			average_categoriesPerSubject, average_subjectsInCategories);
+	END //
 
-DELIMTER ;
+DELIMITER ;
