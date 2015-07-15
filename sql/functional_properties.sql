5674023cc4b2e147e1b25532ac2d83e0b375d34a
tamslo <tamaraslosarek@gmail.com>
2015-07-15 09:05:43 +0100

    Still WIP: Implementing functional properties
    * Stats on number of properties

--- sql/functional_properties.sql
+++ sql/functional_properties.sql
@@ -37,38 +37,21 @@ NOT IN (SELECT DISTINCT predicate_md5 FROM TS_TEST_are_properties_functional_md5
 DROP TABLE IF EXISTS `TS_TEST_property_stats_md5`;
 CREATE TABLE `TS_TEST_property_stats_md5` (
   `predicate_md5` 	varchar(1000) NOT NULL,
-  `predicate_avg`	int NOT NULL,
-  'is_functional'	tinyint(1) NOT NULL 	
+  `predicate_avg`	float NOT NULL,
+  `is_functional`	tinyint(1) NOT NULL 	
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 # !!! NOT WORKING SO FAR, IN PROGRESS !!!
 
 INSERT INTO TS_TEST_property_stats_md5
-SELECT predicate_md5, avg, is_functional
+SELECT avgs.predicate_md5, avg, is_functional
 FROM
 	(SELECT predicate_md5, AVG(count) AS avg
 	FROM
-		(SELECT predicate_md5, COUNT(predicate_md5) AS count
+		(SELECT subject_md5, predicate_md5, COUNT(predicate_md5) AS count
 		FROM statements_md5
 		GROUP BY subject_md5, predicate_md5) counts
 	GROUP BY predicate_md5) avgs
 	INNER JOIN TS_TEST_are_properties_functional_md5 fp
 	ON avgs.predicate_md5 = fp.predicate_md5;
-
-
-
-
-# translate functional props table for testing
-
-DROP TABLE IF EXISTS `TS_TEST_are_properties_functional_clear`;
-CREATE TABLE `TS_TEST_are_properties_functional_clear` (
-  `predicate`		varchar(1000) NOT NULL,
-  `is_functional`	tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO TS_TEST_are_properties_functional_clear
-SELECT trans.predicate, is_functional
-FROM TS_TEST_are_properties_functional_md5 md5
-INNER JOIN predicate_translation trans
-ON md5.predicate_md5 = trans.predicate_md5;
