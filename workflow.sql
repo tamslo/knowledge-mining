7c5616d477ac177284e186274a2596826430b2dd
tamslo <tamaraslosarek@gmail.com>
2015-08-17 16:13:57 +0100

    Renamings in workflow.
    * foo_original -> foo_clear
    * functional_properties -> defined_functional_properties
    * all_rso_translation -> resource_translation
    * subjects_per_category_count_md5 -> subjects_per_category_md5
    * property_probability_md5 -> relative_frequencies_md5
    * are_properties_functional_md5 -> tmp_functional_properties_md5
    * property_stats_md5 -> functional_properties_md5
    * column of functional_properties_md5 is_functional -> defined_functional

--- workflow.sql
+++ workflow.sql
@@ -5,40 +5,40 @@ SET bulk_insert_buffer_size = 256217728;
 
 # Tables to import data
 
-DROP TABLE IF EXISTS `EVAL_categories_original`;
-CREATE TABLE `EVAL_categories_original` (
+DROP TABLE IF EXISTS `EVAL_categories_clear`;
+CREATE TABLE `EVAL_categories_clear` (
   `resource` varchar(1000) NOT NULL,
   `category` varchar(1000) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `EVAL_statements_original`;
-CREATE TABLE `EVAL_statements_original` (
+DROP TABLE IF EXISTS `EVAL_statements_clear`;
+CREATE TABLE `EVAL_statements_clear` (
   `subject` 	varchar(1000) NOT NULL,
   `predicate` 	varchar(1000) NOT NULL,
   `object` 	varchar(1000) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # Tables for suggestion cleansing
 
-DROP TABLE IF EXISTS `EVAL_redirects_original`;
-CREATE TABLE `EVAL_redirects_original` (
+DROP TABLE IF EXISTS `EVAL_redirects_clear`;
+CREATE TABLE `EVAL_redirects_clear` (
   `resource` varchar(1000) NOT NULL,
   `redirect` varchar(1000) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `EVAL_functional_properties`;
-CREATE TABLE `EVAL_functional_properties` (
+DROP TABLE IF EXISTS `EVAL_defined_functional_properties`;
+CREATE TABLE `EVAL_defined_functional_properties` (
   `predicate`	varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/test_categories.csv' INTO TABLE EVAL_categories_original FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/test_statements.csv' INTO TABLE EVAL_statements_original FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/redirects.csv' INTO TABLE EVAL_redirects_original FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
-LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/func_prop.csv' INTO TABLE EVAL_functional_properties FIELDS TERMINATED BY ',' ENCLOSED BY '\"' ESCAPED BY '\\' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/test_categories.csv' INTO TABLE EVAL_categories_clear FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/test_statements.csv' INTO TABLE EVAL_statements_clear FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/redirects.csv' INTO TABLE EVAL_redirects_clear FIELDS TERMINATED BY ',' ENCLOSED BY '\'' ESCAPED BY '\\' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '~/KnowMin/repo/data/func_prop.csv' INTO TABLE EVAL_defined_functional_properties FIELDS TERMINATED BY ',' ENCLOSED BY '\"' ESCAPED BY '\\' LINES TERMINATED BY '\n';
 
 #################################################################################################################################################################################
 
 # Clean data from lists
 
-DELETE FROM EVAL_categories_original WHERE resource LIKE 'http://dbpedia.org/resource/List\_of\_%';
-DELETE FROM EVAL_statements_original WHERE subject LIKE 'http://dbpedia.org/resource/List\_of\_%';
+DELETE FROM EVAL_categories_clear WHERE resource LIKE 'http://dbpedia.org/resource/List\_of\_%';
+DELETE FROM EVAL_statements_clear WHERE subject LIKE 'http://dbpedia.org/resource/List\_of\_%';
 
 #################################################################################################################################################################################
 
@@ -50,7 +50,7 @@ CREATE TABLE `EVAL_categories_md5` (
   `category_md5` char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_categories_md5 SELECT md5(resource),md5(category) FROM  EVAL_categories_original;
+INSERT INTO EVAL_categories_md5 SELECT md5(resource),md5(category) FROM  EVAL_categories_clear;
 
 ALTER TABLE `EVAL_categories_md5` 
 ADD INDEX `idx_categories_md5_resource` (`resource_md5` ASC),
@@ -63,7 +63,7 @@ CREATE TABLE `EVAL_statements_md5` (
   `object_md5`		char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_statements_md5 SELECT md5(subject), md5(predicate), md5(object) FROM EVAL_statements_original;
+INSERT INTO EVAL_statements_md5 SELECT md5(subject), md5(predicate), md5(object) FROM EVAL_statements_clear;
 
 ALTER TABLE `EVAL_statements_md5` 
 ADD INDEX `idx_statements_md5_subject`	(`subject_md5` ASC),
@@ -76,7 +76,7 @@ CREATE TABLE `EVAL_redirects_md5` (
   `redirect_md5` char(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_redirects_md5 SELECT md5(resource),md5(redirect) FROM  EVAL_redirects_original;
+INSERT INTO EVAL_redirects_md5 SELECT md5(resource),md5(redirect) FROM  EVAL_redirects_clear;
 
 ALTER TABLE `EVAL_redirects_md5` 
 ADD INDEX `idx_redirects_md5_resource` (`resource_md5` ASC),
@@ -89,7 +89,7 @@ CREATE TABLE `EVAL_category_translation` (
   PRIMARY KEY 		(`category_md5`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT IGNORE INTO EVAL_category_translation SELECT category,md5(category) FROM EVAL_categories_original;
+INSERT IGNORE INTO EVAL_category_translation SELECT category,md5(category) FROM EVAL_categories_clear;
 
 DROP TABLE IF EXISTS `EVAL_predicate_translation`;
 CREATE TABLE `EVAL_predicate_translation` (
@@ -98,20 +98,20 @@ CREATE TABLE `EVAL_predicate_translation` (
   PRIMARY KEY 		(`predicate_md5`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT IGNORE INTO EVAL_predicate_translation SELECT predicate,md5(predicate) FROM EVAL_statements_original;
+INSERT IGNORE INTO EVAL_predicate_translation SELECT predicate,md5(predicate) FROM EVAL_statements_clear;
 
-DROP TABLE IF EXISTS `EVAL_all_rso_translation`;
-CREATE TABLE `EVAL_all_rso_translation` (
+DROP TABLE IF EXISTS `EVAL_resource_translation`;
+CREATE TABLE `EVAL_resource_translation` (
   `resource` 		varchar(1000) 	NOT NULL,
   `resource_md5` 	char(32) 	NOT NULL,
   PRIMARY KEY 		(`resource_md5`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT IGNORE INTO EVAL_all_rso_translation SELECT resource,md5(resource)	FROM EVAL_categories_original;
-INSERT IGNORE INTO EVAL_all_rso_translation SELECT subject,md5(subject) 	FROM EVAL_statements_original;
-INSERT IGNORE INTO EVAL_all_rso_translation SELECT object,md5(object) 	FROM EVAL_statements_original;
-INSERT IGNORE INTO EVAL_all_rso_translation SELECT resource,md5(resource)  FROM EVAL_redirects_original;
-INSERT IGNORE INTO EVAL_all_rso_translation SELECT redirect,md5(redirect)  FROM EVAL_redirects_original;
+INSERT IGNORE INTO EVAL_resource_translation SELECT resource,md5(resource)	FROM EVAL_categories_clear;
+INSERT IGNORE INTO EVAL_resource_translation SELECT subject,md5(subject) 	FROM EVAL_statements_clear;
+INSERT IGNORE INTO EVAL_resource_translation SELECT object,md5(object) 	FROM EVAL_statements_clear;
+INSERT IGNORE INTO EVAL_resource_translation SELECT resource,md5(resource)  FROM EVAL_redirects_clear;
+INSERT IGNORE INTO EVAL_resource_translation SELECT redirect,md5(redirect)  FROM EVAL_redirects_clear;
 
 #################################################################################################################################################################################
 
@@ -122,7 +122,7 @@ DELETE FROM EVAL_categories_md5 WHERE resource_md5 IN(SELECT resource_md5 FROM E
 
 #################################################################################################################################################################################
 
-# Join categories and statements
+# Join categories and statements with inverted statements
 
 DROP TABLE IF EXISTS `EVAL_cs_join_md5`;
 CREATE TABLE `EVAL_cs_join_md5` (
@@ -169,17 +169,17 @@ CREATE INDEX `idx_cs_join_md5_subject` 	on `EVAL_cs_join_md5`(`subject_md5`);
 
 # Precomputation of intermediate results
 
-DROP TABLE IF EXISTS `EVAL_subjects_per_category_count_md5`;
-CREATE TABLE `EVAL_subjects_per_category_count_md5`(
+DROP TABLE IF EXISTS `EVAL_subjects_per_category_md5`;
+CREATE TABLE `EVAL_subjects_per_category_md5`(
 	`category_md5` 	CHAR(32),
 	`subject_count`	INT
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_subjects_per_category_count_md5 
+INSERT INTO EVAL_subjects_per_category_md5 
 	SELECT 		distinct category_md5, COUNT(distinct resource_md5) 
 	FROM 		EVAL_categories_md5 
 	GROUP BY 	category_md5; 
-CREATE INDEX `idx_spc_category` ON `EVAL_subjects_per_category_count_md5`(`category_md5`);
+CREATE INDEX `idx_spc_category` ON `EVAL_subjects_per_category_md5`(`category_md5`);
 
 DROP TABLE IF EXISTS `EVAL_predicate_object_count_md5`;
 CREATE TABLE `EVAL_predicate_object_count_md5`(
@@ -193,7 +193,7 @@ CREATE TABLE `EVAL_predicate_object_count_md5`(
 INSERT INTO EVAL_predicate_object_count_md5
 	SELECT 		cs.category_md5, cs.predicate_md5, cs.object_md5, COUNT(DISTINCT cs.subject_md5), cs.inverted
 	FROM 		EVAL_cs_join_md5 AS cs
-	LEFT JOIN	EVAL_subjects_per_category_count_md5 AS spc
+	LEFT JOIN	EVAL_subjects_per_category_md5 AS spc
 	ON			cs.category_md5 = spc.category_md5
 	WHERE		spc.subject_count >2
 	GROUP BY	cs.category_md5, cs.predicate_md5, cs.object_md5;
@@ -201,8 +201,8 @@ CREATE INDEX `idx_poc_md5_category` 	ON `EVAL_predicate_object_count_md5`(`categ
 CREATE INDEX `idx_poc_md5_count` 	ON `EVAL_predicate_object_count_md5`(`count`);
 
 
-DROP TABLE IF EXISTS `EVAL_property_probability_md5`;
-CREATE TABLE `EVAL_property_probability_md5`(
+DROP TABLE IF EXISTS `EVAL_relative_frequencies_md5`;
+CREATE TABLE `EVAL_relative_frequencies_md5`(
 	`category_md5`	CHAR(32) NOT NULL,
 	`predicate_md5`	CHAR(32) NOT NULL,
 	`object_md5`	CHAR(32) NOT NULL,
@@ -210,46 +210,46 @@ CREATE TABLE `EVAL_property_probability_md5`(
 	`inverted`	TINYINT(1) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_property_probability_md5 
+INSERT INTO EVAL_relative_frequencies_md5 
 	SELECT 		poc.category_md5, poc.predicate_md5, poc.object_md5, (poc.count/spc.subject_count), poc.inverted
 	FROM 		EVAL_predicate_object_count_md5 	AS poc
-	LEFT JOIN	EVAL_subjects_per_category_count_md5 AS spc
+	LEFT JOIN	EVAL_subjects_per_category_md5 AS spc
 	ON			poc.category_md5 = spc.category_md5;
 
-CREATE INDEX `idx_pp_category` 	ON `EVAL_property_probability_md5`(`category_md5`);
-CREATE INDEX `idx_pp_cpo` 	ON `EVAL_property_probability_md5`(`category_md5`, `predicate_md5`, `object_md5`);
+CREATE INDEX `idx_pp_category` 	ON `EVAL_relative_frequencies_md5`(`category_md5`);
+CREATE INDEX `idx_pp_cpo` 	ON `EVAL_relative_frequencies_md5`(`category_md5`, `predicate_md5`, `object_md5`);
 
 #################################################################################################################################################################################
 
 # Precomputation of functional properties
 
-DROP TABLE IF EXISTS `EVAL_are_properties_functional_md5`;
-CREATE TABLE `EVAL_are_properties_functional_md5` (
+DROP TABLE IF EXISTS `EVAL_tmp_functional_properties_md5`;
+CREATE TABLE `EVAL_tmp_functional_properties_md5` (
   `predicate_md5`	char(32) NOT NULL,
-  `is_functional`	tinyint(1) NOT NULL
+  `defined_functional`	tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_are_properties_functional_md5
+INSERT INTO EVAL_tmp_functional_properties_md5
 SELECT md5(predicate), 1
-FROM EVAL_functional_properties;
+FROM EVAL_defined_functional_properties;
 
 
-INSERT INTO EVAL_are_properties_functional_md5
+INSERT INTO EVAL_tmp_functional_properties_md5
 SELECT DISTINCT predicate_md5, 0
 FROM EVAL_predicate_translation
 WHERE predicate_md5
-NOT IN (SELECT DISTINCT predicate_md5 FROM EVAL_are_properties_functional_md5);
+NOT IN (SELECT DISTINCT predicate_md5 FROM EVAL_tmp_functional_properties_md5);
 
-DROP TABLE IF EXISTS `EVAL_property_stats_md5`;
-CREATE TABLE `EVAL_property_stats_md5` (
+DROP TABLE IF EXISTS `EVAL_functional_properties_md5`;
+CREATE TABLE `EVAL_functional_properties_md5` (
   `predicate_md5` 			varchar(1000) NOT NULL,
-  `predicate_avg`			double NOT NULL,
-  `is_functional`			tinyint(1) NOT NULL,
+  `avg_values`				double NOT NULL,
+  `defined_functional`			tinyint(1) NOT NULL,
   `considered_functional`	tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO EVAL_property_stats_md5
-SELECT avgs.predicate_md5, avg, is_functional, 0
+INSERT INTO EVAL_functional_properties_md5
+SELECT avgs.predicate_md5, avg, defined_functional, 0
 FROM
 	(SELECT predicate_md5, AVG(count) AS avg
 	FROM
@@ -257,14 +257,14 @@ FROM
 		FROM EVAL_statements_md5
 		GROUP BY subject_md5, predicate_md5) counts
 	GROUP BY predicate_md5) avgs
-	INNER JOIN EVAL_are_properties_functional_md5 fp
+	INNER JOIN EVAL_tmp_functional_properties_md5 fp
 	ON avgs.predicate_md5 = fp.predicate_md5
 ORDER BY avg;
 
-UPDATE EVAL_property_stats_md5
+UPDATE EVAL_functional_properties_md5
 SET considered_functional = 1
-WHERE is_functional = 1
-OR predicate_avg = 1;
+WHERE defined_functional = 1
+OR avg_values = 1;
 
 #################################################################################################################################################################################
 
@@ -285,7 +285,7 @@ INSERT INTO EVAL_suggestions_md5
 	SELECT "A" AS status, ca.resource_md5 AS subject_md5, pp.predicate_md5, pp.object_md5, pp.probability, pp.category_md5, pp.inverted
     	FROM
     		(SELECT 	pp.predicate_md5, pp.object_md5, pp.probability, pp.category_md5, pp.inverted
-    			FROM 	EVAL_property_probability_md5 AS pp
+    			FROM 	EVAL_relative_frequencies_md5 AS pp
     			WHERE	pp.probability        >= 0.9
         		AND		pp.probability        < 1) AS pp
     
@@ -324,13 +324,13 @@ FROM EVAL_suggestions_md5
 WHERE EXISTS (SELECT * FROM EVAL_statements_md5 st
 	WHERE EVAL_suggestions_md5.subject_md5 = st.subject_md5
 	AND EVAL_suggestions_md5.predicate_md5 = st.predicate_md5)
-AND EVAL_suggestions_md5.predicate_md5 IN (SELECT predicate_md5 FROM EVAL_property_stats_md5 WHERE considered_functional = 1);
+AND EVAL_suggestions_md5.predicate_md5 IN (SELECT predicate_md5 FROM EVAL_functional_properties_md5 WHERE considered_functional = 1);
 
 DELETE FROM EVAL_suggestions_md5
 WHERE EXISTS (SELECT * FROM EVAL_statements_md5 st
 	WHERE EVAL_suggestions_md5.subject_md5 = st.subject_md5
 	AND EVAL_suggestions_md5.predicate_md5 = st.predicate_md5)
-AND EVAL_suggestions_md5.predicate_md5 IN (SELECT predicate_md5 FROM EVAL_property_stats_md5 WHERE considered_functional = 1);
+AND EVAL_suggestions_md5.predicate_md5 IN (SELECT predicate_md5 FROM EVAL_functional_properties_md5 WHERE considered_functional = 1);
 
 #################################################################################################################################################################################
 
@@ -350,7 +350,7 @@ CREATE TABLE `EVAL_suggestions_clear` (
 INSERT INTO EVAL_suggestions_clear
 	SELECT 		sug.status, s2md5.resource, p2md5.predicate, o2md5.resource, sug.probability, c2md5.category, sug.inverted
 	FROM 		EVAL_suggestions_md5 	AS sug
-	LEFT JOIN 	EVAL_all_rso_translation 	AS s2md5 ON sug.subject_md5 	= s2md5.resource_md5	
+	LEFT JOIN 	EVAL_resource_translation 	AS s2md5 ON sug.subject_md5 	= s2md5.resource_md5	
 	LEFT JOIN 	EVAL_predicate_translation 	AS p2md5 ON sug.predicate_md5 	= p2md5.predicate_md5 	
-	LEFT JOIN 	EVAL_all_rso_translation 	AS o2md5 ON sug.object_md5 	= o2md5.resource_md5	
+	LEFT JOIN 	EVAL_resource_translation 	AS o2md5 ON sug.object_md5 	= o2md5.resource_md5	
 	LEFT JOIN 	EVAL_category_translation	AS c2md5 ON sug.category_md5 	= c2md5.category_md5;
